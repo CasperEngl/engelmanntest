d1ee39be2c95a37dd5a390cb6e475b6df738028f
Casper Engelmann <c.engelmann@custimy.io>
2023-10-24 11:08:35 +0200

    Delete autogenerated files

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1698719533;s:7:"authKey";s:64:"_0tm%]Dgjyp~O,;^hb.SUhqV[8]N=)QOeA4?0BgHKQ|eE!F^5X<.Y%C9>S,#7UX{";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:4089;}
